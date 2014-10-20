2d0ae7dd50e64f211f1ecbe699adcf4eede1025a
mchami <mazenchami@mazens-mbp-2.4implus.local>
2014-10-20 14:31:02 -0400

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // 'declaring' variables
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // function for whenever the send mail button is pressed
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // show hidden message label
+        messageLabel.hidden = false
+        // replace the label text with what is entered in the message text field
+        messageLabel.text = enterMessageTextField.text
+        // changing the message label text color
+        messageLabel.textColor = UIColor.redColor()
+        
+        // emtpy message text field which reshows placeholder functionality
+        enterMessageTextField.text = ""
+        // make the keyboard go away by calling a function
+        enterMessageTextField.resignFirstResponder()
+        
+        // updating the title of the send mail button
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
 
 }
 
